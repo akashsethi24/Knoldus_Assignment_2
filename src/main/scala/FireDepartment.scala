a3854d4ad56efd355d951b389b17ca32c51a173a
Akash Sethi <akashsethi@hotmail.com>
2016-08-04 17:36:35 +0530

    Completed till Q3

--- src/main/scala/FireDepartment.scala
+++ src/main/scala/FireDepartment.scala
@@ -1,6 +1,3 @@
-import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
-
 import org.apache.spark.sql.{DataFrame, Dataset, Encoders, SparkSession}
 
 /**
@@ -11,33 +8,15 @@ object GlobalData {
 
   val spark = SparkSession.builder().appName("Fire Call-On-Service").master("local").getOrCreate()
 
-  implicit val myOrdering = new Ordering[Date] {
-    override def compare(dateOne: Date, dateTwo: Date): Int = {
-      dateOne.getTime.compareTo(dateTwo.getTime)
-    }
-  }
-
-  def getDataSet: DataFrame = {
+  def getDataFrame: DataFrame = {
 
     spark.read.option("header", "true").csv("/home/akash/Documents/Hadoop/Fire_Department_Calls_for_Service.csv")
   }
 
-  import collection.JavaConversions._
-
-  implicit val encoder = Encoders.STRING
-  implicit val dateEncoder = Encoders.DATE
-
-  lazy val maxDate: Date = getDateList.max(myOrdering)
-
   def getSingleColumnData(columnName: String): Dataset[String] = {
 
-    getDataSet.select(columnName).as[String]
-  }
-
-  def getDateList: List[Date] = {
-    val dateDataSet = getSingleColumnData("Call Date")
-    val formatter = new SimpleDateFormat("MM/dd/yyyy")
-    dateDataSet.collectAsList().toList.map { date => formatter.parse(date) }
+    implicit val stringEncoder = Encoders.STRING
+    getDataFrame.select(columnName).as[String]
   }
 
 }
@@ -49,24 +28,26 @@ class FireDepartment {
   def getCallType: List[String] = {
 
     import collection.JavaConversions._
+    implicit val stringEncoder = Encoders.STRING
     getSingleColumnData("Call Type").as[String].distinct().collectAsList().toList
   }
 
   def getYears: Long = {
 
-    import collection.JavaConversions._
-    val dateDataSet = getSingleColumnData("Call Date")
-    val formatter = new SimpleDateFormat("MM/dd/yyyy")
-    val dateList = dateDataSet.collectAsList().toList.map { date => formatter.parse(date) }
-    val minDate = dateList.min(myOrdering)
-    val calender = Calendar.getInstance()
-    calender.setTime(maxDate)
-    val maxYear = calender.get(Calendar.YEAR).toLong
-    calender.setTime(minDate)
-    val minYear = calender.get(Calendar.YEAR).toLong
-    maxYear - minYear
+    import spark.implicits._
+    implicit val intEncoder = Encoders.INT
+    val stringYear = getSingleColumnData("Call Date").map { date => date.split("/")(2).toInt }.distinct()
+    val maxYear = stringYear.reduce((valueOne, valueTwo) => if (valueOne > valueTwo) valueOne else valueTwo)
+    val minYear = stringYear.reduce((valueOne, valueTwo) => if (valueOne < valueTwo) valueOne else valueTwo)
+    maxYear.toLong - minYear.toLong
   }
 
+  def getIncidentWithCallType: Map[String, Long] = {
+
+    import spark.implicits._
+    import collection.JavaConversions._
+    getDataFrame.select("Call Type", "Incident Number").as[(String, String)].groupBy("Call Type").count().as[(String, Long)].collectAsList().toList.toMap
+  }
 }
 
 object FireDepartment extends App {
@@ -74,6 +55,7 @@ object FireDepartment extends App {
   val obj = new FireDepartment
 
   println("Q1. Types Of Call \n" + obj.getCallType)
+  println("Q2. Incident with Call Type " + obj.getIncidentWithCallType)
   println("Q3. Year Of Service = " + obj.getYears)
 
 }
